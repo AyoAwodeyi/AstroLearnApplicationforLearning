43d3317d16c2f39322f79a333d34c28003c77669
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2024-06-21 14:11:05 -0500

    young

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.log(err));
 
-const userRoutes = require("./routes/user");
-app.use("/users", userRoutes);
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our API");
+});
+
+
+
+const connect_string = process.env.CONNECTION_STRING;
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
