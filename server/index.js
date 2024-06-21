ee022933cb651f9ad1839c1603eebd05604bfb28
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2024-06-21 14:41:32 -0500

    databse connection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
-require("dotenv").config();
-
-
+const PORT = process.env.PORT || 3000;
+const connect_string = process.env.CONNECTION_STRING;
 
 
 
-
-
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to our API");
@@ -17,10 +16,15 @@ app.get("/", (req, res) => {
 
 
 
-const connect_string = process.env.CONNECTION_STRING;
-const PORT = process.env.PORT || 3000;
+mongoose
+  .connect(connect_string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Database"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
